770b258cb5ee85c6b26c1239ce643c802eeb55d4
Denys Herasymuk <herasymuk@ucu.edu.ua>
2020-12-13 23:38:48 +0200

    added function to save in files html_texts

--- src/main/java/CacheScraper.java
+++ src/main/java/CacheScraper.java
@@ -1,21 +1,27 @@
 import lombok.SneakyThrows;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.sql.*;
 
 public class CacheScraper implements Scraper {
     private Scraper scraper;
     private final String dbPath = "jdbc:sqlite:urls.db";
     private Statement statement;
+    private int n_html_pages;
+    BufferedWriter writer;
 
     @SneakyThrows
     public CacheScraper(Scraper scraper) {
         this.scraper = scraper;
+        n_html_pages = 0;
         Connection conn = DriverManager.getConnection(dbPath);
         statement = conn.createStatement();
 
         statement.executeUpdate("drop table if exists urls");
-        statement.executeUpdate("create table urls (id integer primary key autoincrement, url string," +
-                " html string)");
+        statement.executeUpdate("create table urls (id integer primary key autoincrement," +
+                " url string)");
     }
 
     @SneakyThrows
@@ -24,7 +30,7 @@ public class CacheScraper implements Scraper {
         boolean found_url = false;
         while (rs.next()) {
             String retrieved_url = rs.getString("url");
-            
+
             if (retrieved_url.equals(url))
                 found_url = true;
         }
@@ -33,23 +39,27 @@ public class CacheScraper implements Scraper {
 
     @SneakyThrows
     public String retrieveCache(String url) {
-        ResultSet rs = statement.executeQuery("select url, html from urls");
-        String found_url_html = "";
+        ResultSet rs = statement.executeQuery("select url from urls");
+        String found_url = "";
         while (rs.next()) {
             String retrieved_url = rs.getString("url");
 
             if (retrieved_url.equals(url))
-                found_url_html = rs.getString("html");
+                found_url = rs.getString("url");
         }
-        return found_url_html;
+        return found_url;
     }
 
     @SneakyThrows
     public void addToCache(String url, String html) {
-//        PreparedStatement pstmt = conn.prepareStatement(SQL,
-//                Statement.RETURN_GENERATED_KEYS);
-//        statement.executeUpdate("insert into urls (url, html) values(?, ?)");
-        statement.executeUpdate("insert into urls (url, html) values(?, ?)");
+        String command = "insert into urls (url) values('" + url + "')";
+        statement.executeUpdate(command);
+
+        String fileName = "html_texts/page" + n_html_pages;
+        writer = new BufferedWriter(new FileWriter(fileName));
+        writer.write(html);
+
+        n_html_pages += 1;
     }
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,9 @@
 import lombok.SneakyThrows;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-//        URLScraper urlScraper = new URLScraper();
-//        System.out.println(urlScraper.retrieve("https://www.google.com/"));
-//        String url = "jdbc:sqlite:urls.db";
-//
-//
-//        statement.executeUpdate("drop table if exists urls");
-//        statement.executeUpdate("create table urls (id integer primary key autoincrement, url string)");
-//        statement.executeUpdate("insert into urls (url) values('http://ucu.edu.ua')");
-//
-//        ResultSet rs = statement.executeQuery("select * from urls");
-//
-//        System.out.println("url = " + rs.getString("url"));
-//        System.out.println("id = " + rs.getString("id"));
         CacheScraper cacheScraper = new CacheScraper(new URLScraper());
 
         String[] targetURLs = new String[] {
@@ -36,6 +19,7 @@ public class Main {
         };
 
         for (String url: targetURLs) {
+            System.out.println("\n Url for searching");
             System.out.println(url);
             cacheScraper.retrieve(url);
         }
